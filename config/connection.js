50cfd5e7af0a5227ae4b3a6f353e97133eb84a6c
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-14 23:28:38 -0500

    add
    database

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // Connecting to the MongoDB database using the MongoDB URI provided in the environment variables or using the default URI if the environment variable is not set
-mongoose.connect(process.env.MONOGDB_URI || 'mongodb://127.0.0.1:27017',{
+mongoose.connect(process.env.MONOGDB_URI || 'mongodb://127.0.0.1:27017/sweat-social',{
     useNewUrlParser: true, 
     useUnifiedTopology: true,
 });
